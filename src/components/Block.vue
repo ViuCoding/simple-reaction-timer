fb6ab12e0d79bdeec8a0e2362fa87ab468d0bb32
Vincenzo- <viulenzgg@gmail.com>
2022-07-11 11:23:56 +0200

    Timer

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">Click Me!</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Me!</div>
 </template>
 
 <script>
@@ -8,12 +8,15 @@ export default {
   data() {
     return {
       showBlock: false,
+      timer: null,
+      reactionTime: 0,
     };
   },
   mounted() {
     console.log("Component has been mounted!");
     setTimeout(() => {
       this.showBlock = true;
+      this.startTimer(); // We start the timer as soon as the Reaction Button appears (which is when showBlock becomes true)
     }, this.delay);
   },
 
@@ -24,6 +27,18 @@ export default {
 
   unmounted() {
     console.log("Unmounted");
+  },
+
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+    },
   },
 };
 </script>
