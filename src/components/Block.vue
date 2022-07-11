2a533fcf8d96523d970d7bbe38a6ca144879c777
Vincenzo- <viulenzgg@gmail.com>
2022-07-11 11:23:43 +0200

    Vue components life cycle hooks + Creating a timer using hooks

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="block">Click Me!</div>
+  <div class="block" v-if="showBlock">Click Me!</div>
 </template>
 
 <script>
 export default {
   props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    console.log("Component has been mounted!");
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
+
+  updated() {
+    // This will fire when data inside the component is updated, in this case when the timer changes showBlock to true.
+    console.log("Component has been updated!");
+  },
+
+  unmounted() {
+    console.log("Unmounted");
+  },
 };
 </script>
 
